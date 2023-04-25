98769fd1d5210e0260fa41e6327f2fa698f8c534
rmessett15 <rmessett15@gmail.com>
2023-04-25 14:22:46 -0500

    commented and formatted all files

--- server.js
+++ server.js
@@ -1,26 +1,35 @@
+// Express import
 const express = require("express");
+// File system module import
 const fs = require("fs");
+// Path import
 const path = require("path");
 // Helper method for generating unique ids
 const uniqid = require("uniqid");
 
+// Port
 const PORT = process.env.PORT || 3001;
 
+// Creates new app with express
 const app = express();
 
+// Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static("Develop/public"));
 
+// Get route which sends back the index.html page
 app.get("/", (req, res) =>
   res.sendFile(path.join(__dirname, "Develop/public/index.html"))
 );
 
+// Get route which sends back the notes.html page
 app.get("/notes", (req, res) =>
   res.sendFile(path.join(__dirname, "Develop/public/notes.html"))
 );
 
+// Get route -> which reads the db.json file and sends back the parsed JSON data
 app.get("/api/notes", function (req, res) {
   fs.readFile("Develop/db/db.json", "utf8", (err, data) => {
     var jsonData = JSON.parse(data);
@@ -29,25 +38,26 @@ app.get("/api/notes", function (req, res) {
   });
 });
 
-// Change variable names
-const readAndAppend = (content, file) => {
+// Reads the newly added notes from the request body and then adds them to the db.json file
+const readThenAppendToJson = (content, file) => {
   fs.readFile(file, "utf8", (err, data) => {
     if (err) {
       console.error(err);
     } else {
       const parsedData = JSON.parse(data);
       parsedData.push(content);
-      writeToFile(file, parsedData);
+      writeNewNoteToJson(file, parsedData);
     }
   });
 };
 
-// Change variable names
-const writeToFile = (destination, content) =>
+// Writes data to db.json -> utilized within the readThenAppendToJson function
+const writeNewNoteToJson = (destination, content) =>
   fs.writeFile(destination, JSON.stringify(content, null, 4), (err) =>
     err ? console.error(err) : console.info(`\nData written to ${destination}`)
   );
 
+// Post route -> receives a new note, saves it to request body, adds it to the db.json file, and then returns the new note to the client
 app.post("/api/notes", (req, res) => {
   const { title, text } = req.body;
   if (title && text) {
@@ -57,7 +67,7 @@ app.post("/api/notes", (req, res) => {
       id: uniqid(),
     };
 
-    readAndAppend(newNote, "Develop/db/db.json");
+    readThenAppendToJson(newNote, "Develop/db/db.json");
 
     const response = {
       status: "success",
@@ -70,6 +80,7 @@ app.post("/api/notes", (req, res) => {
   }
 });
 
+// Delete route -> reads the db.json file, uses the json objects uniqids to match the object to be deleted, removes that object from the db.json file, then re-writes the db.json file
 app.delete("/api/notes/:id", (req, res) => {
   let id = req.params.id;
   let parsedData;
@@ -79,24 +90,13 @@ app.delete("/api/notes/:id", (req, res) => {
     } else {
       parsedData = JSON.parse(data);
       const filterData = parsedData.filter((note) => note.id !== id);
-      writeToFile("Develop/db/db.json", filterData);
+      writeNewNoteToJson("Develop/db/db.json", filterData);
     }
   });
   res.send(`Deleted note with ${req.params.id}`);
 });
 
+// App.listen is used to spin up our local server
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
 );
-
-// Figure out skipping error within application and why once the skip happens I can no longer delete notes either
-// Change variable names from copy and pasted functions and put those functions in the index.js and import/export as needed
-// Do I need a routes folder for only 2 routes?
-// Why when I changes the keys from title and text to noteTitle and noteText does my program now save notes as undefined?
-// Comment and clean up code base (format)
-// Finish readme (6 areas, and delete the ones I don't need)
-// Add git ignore file -> why are the things im putting in gitignore not being ignored (not greyed out?)
-// Figure out how to delete notes
-// DEPLOY APP TO HEROKU!!!
-// DEPLOY APP TO LIVE URL!!!
-// Should the package.json be in the root directory of app?
